c5309052e046f12dd8d9b336f9d56f07dba4c22c
nicomascia <104233444+nicomascia@users.noreply.github.com>
2022-06-09 20:56:29 -0300

    Add files via upload

---
+++ alimentoparaperros.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('aca estaria toda los alimentos para perros');
+});
+
+module.exports = router;
